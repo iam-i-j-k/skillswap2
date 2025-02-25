f33525dc11a9baf2383dda8f8754804526ff3667
Irfan <irfanjankhan7860@gmail.com>
2025-02-25 07:36:59 +0530

    screen

--- src/components/VideoCallPage.jsx
+++ src/components/VideoCallPage.jsx
@@ -8,7 +8,7 @@ const VideoCallPage = () => {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <div className="max-w-7xl">
+      <div className="max-w-screen">
         <VideoCall />
       </div>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
