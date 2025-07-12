3cf1b09a6382238f88a5676624825235e4ec46ad
Irfan <irfanjankhan7860@gmail.com>
2025-07-12 13:33:07 +0530

    Fix PostCSS plugin configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [
@@ -10,8 +8,8 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        tailwindcss,
-        autoprefixer,
+        'tailwindcss',
+        'autoprefixer',
       ],
     },
   },
