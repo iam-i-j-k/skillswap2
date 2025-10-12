541ed68b95250e424a2a9c4c2604dd7fee380282
iam-i-j-k <irfanjankhan7860@gmail.com>
2025-10-12 15:02:39 +0530

    SpeedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { SocketProvider } from './context/SocketContext';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <SocketProvider>
             <App />
+            <SpeedInsights />
         </SocketProvider>
     </Provider>
 );
